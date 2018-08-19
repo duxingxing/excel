44cabd4a198c01dff95449efde4b9a7ebdfbfc8f
duxingxing <820967275@qq.com>
2018-08-19 18:34:51 +0800

    提交

--- poiutil/src/main/java/Test.java
+++ poiutil/src/main/java/Test.java
@@ -1,9 +1,22 @@
+import parseexcel.EventModelParseExcel;
+import parseexcel.ExcelReader;
 import parseexcel.ParseExcel;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2018/8/18.
  */
 public class Test {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
+        String file = "C:\\Users\\Administrator\\Desktop\\test.xlsx";
+        ExcelReader reader = new ExcelReader() {
+            public void getRows(int sheetIndex, int curRow, List<String> rowList) {
+
+                System.out.println("Sheet:" + sheetIndex + ", Row:" + curRow + ", Data:" + rowList);
+
+            }
+        };
+        reader.process(file, 1);
     }
 }
